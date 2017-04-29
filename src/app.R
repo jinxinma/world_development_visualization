ed1c3cf4abf0e51a731c7f308a35339d559d2124
Will Young <wjyoung@dons.usfca.edu>
2017-04-29 12:44:35 -0700

    data reading and shaping functions

--- src/app.R
+++ src/app.R
@@ -2,65 +2,64 @@ rm(list=ls())
 cat('\014')
 
 library(dplyr)
-library(ggvis)
 library(reshape2)
-library(shiny)
+library(tidyr)
 
-setwd('/Users/Will/msan/vis/project/src/')
+setwd('/Users/Will/msan/vis/world_data_vis/src/')
+
+
+clean_year_string <- function(df) {
+  df$Year <- lapply(df$Year, function(s) substr(s, 2, 5))
+  return(df)
+}
+
+
+format_for_heat <- function(df) {
+  df <- melt_years(df)
+  return(df)
+}
+
 
 format_for_parallel <- function(df) {
-  df$Fertility.Rate <- normalize_list(df$Fertility.Rate)
-  df$Life.Expectancy <- - normalize_list(df$Life.Expectancy)
-  df$Population <- normalize_list(df$Population)
-  df$Fertility.Rate2 <- - normalize_list(df$Fertility.Rate)
-  df$Life.Expectancy2 <- normalize_list(df$Life.Expectancy)
-  df$Population2 <- - normalize_list(df$Population)
-  return(melt(df, c('Country.Name', 'Year')))
+  df <- melt_years(df)
+  df <- df[, names(df) != 'Indicator.Name']
+  df <- spread(df, Indicator.Code, value)
+  df <- group_by(df, Country.Code) %>% 
+    mutate_each(funs(normalize_list), c(-Year, -Country.Name))
+  df <- melt(df, c('Country.Name', 'Country.Code', 'Year'))
+  return(df)
+}
+
+
+format_for_time_series <- function(df) {
+  df <- melt_years(df)
+  df <- df[, names(df) != 'Indicator.Name']
+  df <- spread(df, Indicator.Code, value)
+  return(df)
 }
 
 
 load_data <- function() {
-  fert_df <- read_data("../API_SP/API_SP.DYN.TFRT.IN_DS2_en_csv_v2.csv", 4)
-  le_df <- read_data("../API_SP-2/API_SP.DYN.LE00.IN_DS2_en_csv_v2.csv", 4)
-  pop_df <- read_data("../API_SP.POP.TOTL_DS2_en_csv_v2.csv")
-  cont_df <- read.csv("../continents.csv", stringsAsFactors = FALSE)
-  
-  fert_df <- reshape_df(fert_df, "Fertility.Rate")
-  le_df <- reshape_df(le_df, "Life.Expectancy")
-  pop_df <- reshape_df(pop_df, "Population")
-  
-  df_temp <- merge(cont_df, fert_df, by = c("Country.Name"), all.x = TRUE, all.y = FALSE)
-  df_temp <- merge(df_temp, le_df, by = c("Country.Name", "Year"))
-  df <- merge(df_temp, pop_df, by = c("Country.Name", "Year"))
-  return (merge(df_temp, pop_df, by = c("Country.Name", "Year")))
+  df <- read_data("../WDI_csv/WDIData.csv")
+  df <- df[df$Indicator.Code %in% c('PA.NUS.PPP.05', 'PA.NUS.PRVT.PP.05'), ]
+  return(df)
 }
 
 
-normalize <- function(x, minimum, maximum) {
-  return ((x - minimum) / (maximum - minimum))
+melt_years <- function(df) {
+  df <- melt(df, c('Country.Name', 'Country.Code', 'Indicator.Name', 'Indicator.Code'))
+  df <- plyr::rename(df, c("variable" = "Year"))
+  df <- clean_year_string(df)
+  return (df)
 }
 
 
 normalize_list <- function(l) {
-  return(vapply(l, FUN = normalize, FUN.VALUE = 1, min(l), max(l)))
+  (l - min(l)) / (max(l) - min(l))
 }
 
 
 read_data <- function(file_name, skip=0) {
   df <- read.csv(file_name, skip=skip, stringsAsFactors = FALSE)
-  df <- df[, -c(2,3,4,60, 61, 62)]
   return (df)
 }
-
-
-reshape_df <- function(df, value) {
-  df <- melt(df, measure.vars = names(df)[2:length(names(df))])
-  df <- plyr::rename(df, c("variable" = "Year", "value" = value))
-  df$Year <- lapply(df$Year, function(s) substr(s, 2, 5))
-  return (df)
-}
-
-
-
-
-
