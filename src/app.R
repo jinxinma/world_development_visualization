a898d608f6cb2fbe293b199b59f0a82cfc8f91b3
jinxin <jma33@dons.usfca.edu>
2017-04-29 21:23:35 -0700

    adjust slider bar

--- src/app.R
+++ src/app.R
@@ -6,37 +6,68 @@ library(shiny)
 source('data_wrangle.R')
 source('plotting.R')
 
-bind_heat <- function(df) {
-  plot_heat(df) %>% bind_shiny("heat", "p_ui")
+world_df <- load_data()
+world_df <- world_df[world_df$Indicator.Code %in% c("SP.DYN.TFRT.IN",
+                                                    "NV.AGR.TOTL.ZS",
+                                                    "EG.USE.ELEC.KH.PC"), ]
+
+bind_heat <- function(df, year) {
+  df %>%
+     filter(Year == year) %>%
+     plot_heat()
 }
 
-bind_pulse <- function(df) {
-  plot_pulse(df) %>% bind_shiny("pulse", "p_ui")
+bind_pulse <- function(df, country_select, year) {
+  df %>%
+    filter(Country.Name == country_select) %>%
+    filter(Year == year) %>%
+    plot_pulse()
 }
-
 
 ui <- fluidPage(
   titlePanel("Project"),
   mainPanel(
     tabsetPanel(
-      tabPanel("World Heat Map", ggvisOutput('heat')),
+      tabPanel("World Heat Map", 
+               sliderInput("year",
+                           label = "Year: ",
+                           min = 1961, 
+                           max = 2014, 
+                           value = 1961, 
+                           animate = animationOptions(interval = 500, loop = TRUE)
+               ),
+               ggvisOutput('heat'),
+               uiOutput("h_ui")),
+      
       #tabPanel("River Time Series", ),
-      tabPanel("World Pulse", ggvisOutput('pulse'))
+      tabPanel("World Pulse", 
+               sliderInput("year",
+                           label = "Year: ",
+                           min = 1961, 
+                           max = 2014, 
+                           value = 1961, 
+                           animate = animationOptions(interval = 100, loop = TRUE)
+               ),
+               selectInput("country",
+                           label = "Country: ",
+                           choices = world_df$Country.Name, 
+                           multiple = FALSE, 
+                           selected = NA),
+               ggvisOutput('pulse'),
+               uiOutput("p_ui"))
     )
-  ),
-  uiOutput("p_ui")
+  )
 )
 
 server <- function(input, output) {
-  world_df <- load_data()
-  world_df <- world_df[world_df$Indicator.Code %in% c("SP.DYN.TFRT.IN",
-                                                      "NV.AGR.TOTL.ZS",
-                                                      "EG.USE.ELEC.KH.PC"), ]
   parallel_df <- format_for_parallel(world_df)
   heat_df <- format_for_heat(world_df)
   
-  bind_pulse(parallel_df)
-  bind_heat(heat_df)
+  vis_pulse <- reactive({parallel_df %>% bind_pulse(input$country, input$year)})
+  vis_pulse %>% bind_shiny("pulse", "p_ui")
+
+  vis_heat <- reactive({heat_df %>% bind_heat(input$year)})
+  vis_heat %>% bind_shiny("heat", "h_ui") 
 }
 
 
